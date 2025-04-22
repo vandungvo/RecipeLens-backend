af6b966b97a163fbcbb44d41835a2b6eec9c1330
Dung Vo <90669649+vandungvo@users.noreply.github.com>
2025-04-22 11:24:31 +0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -41,6 +41,18 @@ async function bootstrap() {
     ],
   });
   app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', req.headers.origin || '*');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  
+    if (req.method === 'OPTIONS') {
+      return res.sendStatus(204); // CORS preflight OK
+    }
+  
+    next();
+  });
   await app.listen(3000);
 }
 bootstrap();
