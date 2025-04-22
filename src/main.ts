d095f4ce9d251ca0c4b55b8caeeb6bd060f2576f
Dung Vo <90669649+vandungvo@users.noreply.github.com>
2025-04-22 10:27:44 +0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -9,8 +9,6 @@ async function bootstrap() {
 
   app.enableCors({
     origin: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
   });
   
